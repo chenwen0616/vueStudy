c9c8a8dc428e93c1fdae6d2de33129e410b8942c
chenwen0616 <951867067@qq.com>
2021-11-19 17:22:24 +0800

    first commit

---
+++ vue_basic/12.单文件组件/School.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 组件结构 -->
+  <div class="demo">
+    <h2>学校名称：{{schoolName}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+  // 组件交互脚本
+  const school = {
+    name: 'School',
+    data(){
+      return{
+        schoolName: '白云区学校',
+        address: '白云区'
+      }
+    },
+    methods:{
+      showName(){
+        console.log(this.schoolName)
+      }
+    }
+  }
+  export default school
+</script>
+
+<style>
+/* 组件样式 */
+.demo{
+  background: orange
+}
+</style>
---
+++ vue_basic/12.单文件组件/Student.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 组件结构 -->
+  <div class="demo">
+    <h2>姓名：{{stuName}}</h2>
+    <h2>年龄：{{age}}</h2>
+    <button @click="showName">点我提示姓名</button>
+  </div>
+</template>
+
+<script>
+  // 组件交互脚本
+  const student = {
+    name: 'Student',
+    data(){
+      return{
+        stuName: 'dd',
+        age: 12
+      }
+    },
+    methods:{
+      showName(){
+        console.log(this.stuName)
+      }
+    }
+  }
+  export default student
+</script>
+
+<style>
+/* 组件样式 */
+.demo{
+  background: orange
+}
+</style>
